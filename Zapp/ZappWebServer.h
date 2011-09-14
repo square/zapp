fcfb7fbd987cd44711e998f7071e414a88fa721c
Jim Puls <jim@nondifferentiable.com>
2011-09-13 23:46:51 -0700

    Boilerplate at the tops of files

--- Zapp/ZappWebServer.h
+++ Zapp/ZappWebServer.h
@@ -3,8 +3,9 @@
 //  Zapp
 //
 //  Created by Jim Puls on 8/21/11.
-//  Copyright (c) 2011 Square, Inc. All rights reserved.
-//
+//  Licensed to Square, Inc. under one or more contributor license agreements.
+//  See the LICENSE file distributed with this work for the terms under
+//  which Square, Inc. licenses this file to you.
 
 #import <Foundation/Foundation.h>
 
