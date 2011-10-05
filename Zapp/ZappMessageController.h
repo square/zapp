aaf8d5f32ecc50f58e8b1162a42ef9844175b1d5
Zach Margolis <margolis@squareup.com>
2011-10-05 14:29:39 -0700

    Lay out controller

---
+++ Zapp/ZappMessageController.h
@@ -0,0 +1,16 @@
+//
+//  ZappMessageController.h
+//  Zapp
+//
+//  Created by Zach Margolis on 10/5/11.
+//  Copyright (c) 2011 Square, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZappMessageController : NSObject
+
+- (void)sendMessageForLatestBuildInRepository:(ZappRepository *)repository;
+- (void)sendMessageForBuild:(ZappBuild *)build;
+
+@end
---
+++ Zapp/ZappMessageController.m
@@ -0,0 +1,22 @@
+//
+//  ZappMessageController.m
+//  Zapp
+//
+//  Created by Zach Margolis on 10/5/11.
+//  Copyright (c) 2011 Square, Inc. All rights reserved.
+//
+
+#import "ZappMessageController.h"
+
+@implementation ZappMessageController
+
+- (void)sendMessageForLatestBuildInRepository:(ZappRepository *)repository;
+{
+    
+}
+
+- (void)sendMessageForBuild:(ZappBuild *)build;
+{
+}
+
+@end
